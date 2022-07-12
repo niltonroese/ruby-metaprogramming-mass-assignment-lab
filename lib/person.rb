3a1df7d12c243f34ef09d35ea0cec7a46fc59ac9
Nilton <niltonroese@gmail.com>
2022-07-12 19:43:05 +1000

    fix argument

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 # class Person
-#     def initialize(attributes)
+#     def initialize(attributes={})
 #       attributes.each do |key, value| 
-#         self.class.attr_accessor(key)
-#         self.send(("#{key}="), value)
+#         self.class.attr_accessor(key) #setting the setter keys
+#         self.send(("#{key}="), value) #accessing values on setted keys
 #       end
 #     end
 #   end
 
   class Person
-    # we don't want to do this anymore :(
     attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
     :weight, :handed, :complexion, :t_shirt_size, 
     :wrist_size, :glove_size, :pant_length, :pant_width
+    #better to use like this in order to restrict attributes to be used.
   
     def initialize(attributes)
       attributes.each {|key, value| self.send(("#{key}="), value)}
